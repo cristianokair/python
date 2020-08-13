e9e60a14cde06edf2528ba590c9627d528545739
Cristiano Kair <cristianokair@hotmail.com>
2020-08-13 18:48:11 -0300

    Atividade UTFPR

--- exemplo_prático_com_pandas.py
+++ exemplo_prático_com_pandas.py
@@ -19,3 +19,45 @@ plt.ylabel('some numbers')
 plt.show()
 
 # df_DV.plot('date', ['last_available_confirmed', 'last_available_deaths'])
+
+
+# código da atividade em grupo
+df_completo = pd.read_csv("covid19_casos_brasil.csv")
+df_completo.head()
+
+colsConfirmados = ['city', 'date', 'last_available_confirmed', 'new_confirmed']
+colsMortes = ['city', 'date', 'last_available_deaths', 'new_deaths']
+cidades = ['Joinville', 'Francisco Beltrão', 'Dois Vizinhos', 'Pato Branco', 'Florianópolis']
+frames = list()
+for c in cidades:
+    frames.append(df_completo[df_completo['city'] == c])
+df = pd.concat(frames)
+df[colsConfirmados]
+
+df_confirmados = df[colsConfirmados]
+df_mortes = df[colsMortes]
+
+df_confirmados.head()
+
+df_mortes.head()
+
+filtro_cidades = (df_completo['city'] == 'Joinville') | (df_completo['city'] == 'Francisco Beltrão') | (df_completo['city'] == 'Dois Vizinhos') | (df_completo['city'] == 'Pato Branco') | (df_completo['city'] == 'Florianópolis')
+df_DV = df_completo[filtro_cidades]
+df_DV.tail()[colsConfirmados]
+
+filtro_J = (df_completo['city'] == 'Dois Vizinhos') & (df_completo['date'] == '2020-07-10')
+df_J = df_completo[filtro_J]
+df_J[colsConfirmados]
+
+for i in cidades:
+    df_J = df_confirmados[df_completo['city'] == i]
+    plt.plot(df_J.date, df_J.last_available_confirmed)
+
+plt.show()
+
+for i in cidades:
+    filtro = df_completo['city'] == i
+    df_J = df_confirmados[filtro]
+    plt.plot(df_J.date, df_J.new_confirmed)
+
+plt.show()
