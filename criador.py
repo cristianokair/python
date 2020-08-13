5e6cfd55c2b2a006b5b14840b8a1dd68b8cd31fd
Cristiano Kair <cristianokair@hotmail.com>
2020-08-13 19:10:15 -0300

    Atividade UTFPR carregador

--- criador.py
+++ criador.py
@@ -1,6 +1,5 @@
-import pandas as pd
-import numpy as np
 import psycopg2
+
 %matplotlib inline
 plt.rcParams["figure.figsize"] = [15, 15]
 
@@ -10,7 +9,7 @@ usuario = "csk"
 senha = "csk123"
 porta = 5432
 
-def criarBD(hostName, dbName, userName, pw, port):
+def conectarBD(hostName, dbName, userName, pw, port):
     try:
         conn = psycopg2.connect(
             host = hostName,
@@ -25,45 +24,12 @@ def criarBD(hostName, dbName, userName, pw, port):
     
     return conn
 
-conn = criarBD( host, nomeBanco, usuario, senha, porta )
+conn = conectarBD( host, nomeBanco, usuario, senha, porta )
 
 cur = conn.cursor()
 
-df_completo = pd.read_csv("covid19_casos_brasil.csv")
-
 cur.execute("CREATE TABLE IF NOT EXISTS cidade (id serial PRIMARY KEY, city_ibge_code integer, city varchar, state varchar);")
 cur.execute("CREATE TABLE IF NOT EXISTS casos (id serial PRIMARY KEY, city_ibge_code integer, date last_available_confirmed integer, last_available_deaths integer, new_confirmed integer, new_deaths integer);")
-conn.commit()
-
-colsCidade = ['city_ibge_code', 'city', 'state']
-filtro_cidades = (df_completo['city'] == 'Joinville') | (df_completo['city'] == 'Francisco Beltrão') | (df_completo['city'] == 'Dois Vizinhos') | (df_completo['city'] == 'Pato Branco') | (df_completo['city'] == 'Florianópolis')
-df_completo_cidade = df_completo[filtro_cidades][colsCidade]
-df_cidade = df_completo_cidade.tail()
-
-for index, row in df_cidade.iterrows():
-    cur.execute("""
-        INSERT INTO cidade (city_ibge_code, city, state)
-        VALUES (%(a)s, %(b)s, %(c)s);
-        """,{'a': row['city_ibge_code'],
-             'b': row['city'],
-             'c': row['state']
-            })
-conn.commit()
-
-colsCasos = ['city_ibge_code', 'last_available_confirmed', 'last_available_deaths', 'new_confirmed', 'new_deaths']
-df_completo_casos = df_completo[filtro_cidades][colsCasos]
-df_cidade = df_completo_casos
-
-for index, row in df_cidade.iterrows():
-    cur.execute("""
-        INSERT INTO casos (city_ibge_code, last_available_confirmed, last_available_deaths, new_confirmed, new_deaths)
-        VALUES (%(a)s, %(b)s, %(c)s, %(d)s, %(e)s);
-        """,{'a': row['city_ibge_code'],
-             'b': row['last_available_confirmed'],
-             'c': row['last_available_deaths'],
-             'd': row['new_confirmed'],
-             'e': row['new_deaths']
-            })
 conn.commit()
 cur.close()
 conn.close()
